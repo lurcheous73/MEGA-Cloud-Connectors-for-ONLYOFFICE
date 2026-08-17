12c39856d2fed9a796c86aaf77ca05449bbaf730
Christopher Swain <rpj27n2xgd@privaterelay.appleid.com>
2026-08-17 12:05:55 +0100

    Add Brimstone MEGA Cloud security DAO

---
+++ src/mega-cloud/communityserver-12.8/BrimstoneMegaCloud/BrimstoneMegaCloudSecurityDao.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+using ASC.Files.Core;
+using ASC.Files.Core.Security;
+
+namespace ASC.Files.Thirdparty.BrimstoneMegaCloud
+{
+    internal sealed class BrimstoneMegaCloudSecurityDao : BrimstoneMegaCloudDaoBase, ISecurityDao
+    {
+        public BrimstoneMegaCloudSecurityDao(BrimstoneMegaCloudDaoSelector.BrimstoneMegaCloudInfo info,
+                                             BrimstoneMegaCloudDaoSelector selector)
+            : base(info, selector)
+        {
+        }
+
+        public void SetShare(FileShareRecord record) { }
+        public IEnumerable<FileShareRecord> GetShares(IEnumerable<Guid> subjects) { return null; }
+        public IEnumerable<FileShareRecord> GetShares(IEnumerable<FileEntry> entries) { return null; }
+        public IEnumerable<FileShareRecord> GetShares(FileEntry entry) { return null; }
+        public void RemoveSubjects(IEnumerable<Guid> subjects) { }
+        public IEnumerable<FileShareRecord> GetPureShareRecords(IEnumerable<FileEntry> entries) { return null; }
+        public IEnumerable<FileShareRecord> GetPureShareRecords(FileEntry entry) { return null; }
+        public void DeleteShareRecords(IEnumerable<FileShareRecord> records) { }
+        public bool IsShared(object entryId, FileEntryType type) { return false; }
+    }
+}
